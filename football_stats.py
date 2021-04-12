b31284dd7771717a7365e76c3710e6828c14f17c
irchorn <70407458+irchorn@users.noreply.github.com>
2021-04-11 22:32:14 -0400

    Add files via upload

---
+++ football_stats.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from bs4 import BeautifulSoup
+import csv
+
+soup = BeautifulSoup (open("2020_NFL.htm"), features="lxml")
+print(soup.prettify())
+
+
+# In[3]:
+
+
+list(soup.children)
+
+
+# In[4]:
+
+
+[type(item) for item in list(soup.children)]
+
+
+# In[5]:
+
+
+trs = soup.find_all('tr')
+for tr in trs:
+    print(tr)
+
+
+# In[6]:
+
+
+top_players = soup.find_all('tr')[1:21]
+for player in top_players:
+    names = player.contents[0]
+    
+    print(names)
+    
+
+
+# In[7]:
+
+
+position = soup.find_all('span', class_='CellPlayerName-position')[1:21]
+print(position)
+
+
+# In[8]:
+
+
+team = soup.find_all('span', class_='CellPlayerName-team')[1:21]
+print(team)
+
+
+# In[9]:
+
+
+score = soup.find_all('td', class_='TableBase-bodyTd TableBase-bodyTd--number')
+print(score)
+
+
+# In[10]:
+
+
+file = csv.writer(open("NFL_2020.csv", "w"))
+file.writerow(["Names", "TD") 
+
+top_players = soup.find_all('tr')[1:21]
+for player in top_players:
+    names = player.contents[0]
+    
+    print(names)
+    file.writerow(names)
+
+
+# In[11]:
+
+
+if __name__ == "__main__":
+    pass
+
+
+# In[ ]:
+
+
+
+
