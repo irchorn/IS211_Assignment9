19ddeccaaccd965b792a852b13e43517aedde218
irchorn <70407458+irchorn@users.noreply.github.com>
2021-04-17 10:52:42 -0400

    Update football_stats.py
    edited

--- football_stats.py
+++ football_stats.py
@@ -1,80 +1,29 @@
-#!/usr/bin/env python
-# coding: utf-8
+import requests
+from bs4 import BeautifulSoup
 
-# In[2]:
+NFL_URL = "https://www.cbssports.com/nfl/stats/player/passing/nfl/regular/qualifiers/"
+
+
+def main():
+    response = requests.get(NFL_URL)
+    soup = BeautifulSoup(response.text, features="lxml")
+
+    # Specify details of the rows I want
+    rows = soup.find_all('tr', class_="TableBase-bodyTr")
+
+    # player, team, position, touchdowns
+    for n, row in enumerate(rows):
+        if n > 19:
+            break
+        cells = row.find_all("td")
+        player = cells[0].find('span', class_="CellPlayerName--long")
+        name = player.find('a').text.strip()
+        position = player.find('span', class_="CellPlayerName-position").text.strip()
+        team = player.find('span', class_="CellPlayerName-team").text.strip()
+
+        touchdowns = cells[8].text.strip()
+        print(f"{name} plays {position} for {team} with {touchdowns} TDs")
 
 
 if __name__ == "__main__":
-    from bs4 import BeautifulSoup
-    import csv
-
-    soup = BeautifulSoup (open("2020_NFL.htm"), features="lxml")
-    print(soup.prettify())
-
-
-# In[3]:
-
-
-    list(soup.children)
-
-
-# In[4]:
-
-
-    [type(item) for item in list(soup.children)]
-
-
-# In[5]:
-
-
-    trs = soup.find_all('tr')
-    for tr in trs:
-        print(tr)
-
-
-# In[6]:
-
-
-    top_players = soup.find_all('tr')[1:21]
-    for player in top_players:
-        names = player.contents[0]
-        print(names)
-    
-
-
-# In[7]:
-
-
-    position = soup.find_all('span', class_='CellPlayerName-position')[1:21]
-    print(position)
-
-
-# In[8]:
-
-
-    team = soup.find_all('span', class_='CellPlayerName-team')[1:21]
-    print(team)
-
-
-# In[9]:
-
-
-    score = soup.find_all('td', class_='TableBase-bodyTd TableBase-bodyTd--number')
-    print(score)
-
-
-# In[10]:
-
-
-    file = csv.writer(open("NFL_2020.csv", "w"))
-    file.writerow(["Names", "TD"]) 
-
-    top_players = soup.find_all('tr')[1:21]
-    for player in top_players:
-        names = player.contents[0]
-        print(names)
-        file.writerow(names)
-
-
-
-
+    main()
